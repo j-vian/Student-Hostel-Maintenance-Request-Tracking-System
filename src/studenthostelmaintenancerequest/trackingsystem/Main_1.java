0ed527b1376625939a7f8cd746004d76bb9845ac
vian <>
2026-04-08 15:24:23 +0800

    Initial Commit - Project Setup

--- src/studenthostelmaintenancerequest/trackingsystem/Main_1.java
+++ src/studenthostelmaintenancerequest/trackingsystem/Main_1.java
@@ -14,7 +14,6 @@ public class Main_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Try");
         // TODO code application logic here
     }
     
