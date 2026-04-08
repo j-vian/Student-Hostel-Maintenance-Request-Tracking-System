bfc65511d65d1320b738f661c55445927eeac57b
vian <>
2026-04-08 15:34:48 +0800

    Initial Commit - Project Setups

--- src/studenthostelmaintenancerequest/trackingsystem/Main_1.java
+++ src/studenthostelmaintenancerequest/trackingsystem/Main_1.java
@@ -16,8 +16,6 @@ public class Main_1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println();
-        System.out.println("try");
-        System.out.println("thanks");
     }
     
 }
