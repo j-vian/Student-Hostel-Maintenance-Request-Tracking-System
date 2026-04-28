d1d736d8850c039b5a9ebecaced26b6d5d3d8df1
vian <vian@LAPTOP-BTRT0AS0>
2026-04-28 21:42:06 +0800

    Initial Commit - Project Setups

---
+++ src/studenthostelmaintenancerequest/trackingsystem/Status.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studenthostelmaintenancerequest.trackingsystem;
+
+/**
+ *
+ * @author vian
+ */
+public enum Status { //ENUMERATION CLASS: Status
+    SUBMITTED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
