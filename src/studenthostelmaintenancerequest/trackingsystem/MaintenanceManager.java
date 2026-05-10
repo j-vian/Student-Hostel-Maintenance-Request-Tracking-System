e8d797089822e696318bfb49f06169aa20f3c2c3
vian <vian@LAPTOP-BTRT0AS0>
2026-05-10 18:07:15 +0800

    MaintenanceManager - methods done

--- src/studenthostelmaintenancerequest/trackingsystem/MaintenanceManager.java
+++ src/studenthostelmaintenancerequest/trackingsystem/MaintenanceManager.java
@@ -11,34 +11,60 @@ package studenthostelmaintenancerequest.trackingsystem;
 public class MaintenanceManager {
     //data variable
     private MaintenanceRequest[] requests;
+    private int count; 
     
     //constructor
     public MaintenanceManager() {
         this.requests = new MaintenanceRequest[100];
+        this.count = 0;
     }
     
     //addRequest method
-    public void addRequest() {
-        
+    public void addRequest(MaintenanceRequest request) {
+        if (count < requests.length) {
+            requests[count] = request;
+            count++;
+            System.out.println("Request [" + request.getRequestId() + "] added successfully.");
+        }
     }
     
     //assignStaff method
-    public void assignStaff() {
-        
+    public void assignStaff(String requestId, Staff staff) {
+        for (int i = 0; i < count; i++) {
+            if (requests[i].getRequestId().equals(requestId)) {
+                requests[i].setStatus(Status.IN_PROGRESS);
+                System.out.println("Staff [" + staff.getName() + "] assigned to Request " + requestId + "]. Status updated to IN_PROGRESS.");
+                return;
+            }
+        }
+        System.out.println("Request [" + requestId + "] not founc. Cannot assign staff.");
     }
     
     //updateRequestStatus method
-    public void updateRequestStatus() {
-        
+    public void updateRequestStatus(String requestId, Status newStatus) {
+        for (int i = 0; i < count; i++) {
+            if (requests[i].getRequest().equals(requestId)) {
+                requests[i].setStatus(newStatus);
+                System.out.println("Request [" + requestId + "] status updated to: " + newStatus);
+                return;
+            }
+        }
+        System.out.println("Request [" + requestId + "] not found. Status not updated.");
     }
     
     //searchRequest method
-    public void searchRequest() {
-        
+    public void searchRequest(String requestId) {
+        for (int i = 0; i < count; i++) {
+            if (requests[i].getRequestId().equals(requestId)) {
+                System.out.println("Request found:");
+                requests[i].displayDetails();
+                return;
+            }
+        }
+        System.out.println("Request [" + requestId + "] not found in the system.");
     }
     
     //getter
-    public MaintenanceRequest[] getRequest() {
-        return requests;
-    }
+    public MaintenanceRequest[] getRequests() { return requests; }
+    public int getCount() { return count; }
 }
