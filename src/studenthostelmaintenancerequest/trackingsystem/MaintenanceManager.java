b7244f4a11ac8462a75c027f2033f59c5d3f0032
vian <vian@LAPTOP-BTRT0AS0>
2026-05-10 17:27:57 +0800

    MaintenanceManager - methods x done

---
+++ src/studenthostelmaintenancerequest/trackingsystem/MaintenanceManager.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studenthostelmaintenancerequest.trackingsystem;
+
+/**
+ *
+ * @author vian
+ */
+public class MaintenanceManager {
+    //data variable
+    private MaintenanceRequest[] requests;
+    
+    //constructor
+    public MaintenanceManager() {
+        this.requests = new MaintenanceRequest[100];
+    }
+    
+    //addRequest method
+    public void addRequest() {
+        
+    }
+    
+    //assignStaff method
+    public void assignStaff() {
+        
+    }
+    
+    //updateRequestStatus method
+    public void updateRequestStatus() {
+        
+    }
+    
+    //searchRequest method
+    public void searchRequest() {
+        
+    }
+    
+    //getter
+    public MaintenanceRequest[] getRequest() {
+        return requests;
+    }
+}
