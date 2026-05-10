1e0eefe88b83c85859f02c44602e023332407c79
vian <vian@LAPTOP-BTRT0AS0>
2026-05-10 19:34:55 +0800

    MaintenanceFactory - done (change if else to switch case)

--- src/studenthostelmaintenancerequest/trackingsystem/MaintenanceFactory.java
+++ src/studenthostelmaintenancerequest/trackingsystem/MaintenanceFactory.java
@@ -13,21 +13,22 @@ public class MaintenanceFactory {
     public MaintenanceRequest createRequest(String type, String requestId, String description, String priority) {
         String normalizedType = type.toLowerCase();
         
-        if (normalizedType.equals("electrical")) {
-            System.out.println("Factory: Creating ElectricalRequest [" + requestId + "]...");
-            return new ElectricalRequest(requestId, description, priority);
-        }
-        else if (normalizedType.equals("plumbing")) {
-            System.out.println("Factory: Creating PlumbingRequest [" + requestId + "]...");
-            return new PlumbingRequest(requestId, description, priority);
-        }
-        else if (normalizedType.equals("furniture")) {
-            System.out.println("Factory: Creating FurnitureRequest [" + requestId + "]...");
-            return new FurnitureRequest(requestId, description, priority);
-        }
-        else {
-            System.out.println("Factory: Unknown request type [" + type + "]. Returning null.");
-            return null;
+        switch (normalizedType) {
+            case "electrical":
+                System.out.println("Factory: Creating ElectricalRequest [" + requestId + "]...");
+                return new ElectricalRequest(requestId, description, priority);
+
+            case "plumbing":
+                System.out.println("Factory: Creating PlumbingRequest [" + requestId + "]...");
+                return new PlumbingRequest(requestId, description, priority);
+
+            case "furniture":
+                System.out.println("Factory: Creating FurnitureRequest [" + requestId + "]...");
+                return new FurnitureRequest(requestId, description, priority);
+
+            default:
+                System.out.println("Factory: Unknown request type [" + type + "]. Returning null.");
+                return null;
         }
     }
 }
